6def33021ab4f30ed08a3d60abf5d5e29e18b103
azeemnow <nowazeem@gmail.com>
2015-09-14 20:41:18 -0400

    Updated some code-style to remain consistent with the rest of the code.

--- RemoveLeadingSlash/RemoveLeadingSlash.py
+++ RemoveLeadingSlash/RemoveLeadingSlash.py
@@ -1,10 +1,8 @@
 import csv
-DomainList = []
 
 domains = open('domainlist.csv', 'r')
-DomainList = csv.reader(domains)
-DomainList = [column[1] for column in DomainList]
-strip_list = [item.rstrip('/') for item in DomainList]
-print('\n'.join(strip_list))
-
+DomainListCSV = csv.reader(domains)
+DomainList = [column[1] for column in DomainListCSV]
+StripList = [item.rstrip('/') for item in DomainList]
+print('\n'.join(StripList))
 
