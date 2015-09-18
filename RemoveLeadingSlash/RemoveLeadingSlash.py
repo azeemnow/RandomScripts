0f1c4192437e45ed46d73150b7983c04a4f561a0
azeemnow <nowazeem@gmail.com>
2015-09-17 21:19:10 -0400

    Added Comments

--- RemoveLeadingSlash/RemoveLeadingSlash.py
+++ RemoveLeadingSlash/RemoveLeadingSlash.py
@@ -1,8 +1,11 @@
 import csv
 
+#open csv file with list of domain in read only
 domains = open('domainlist.csv', 'r')
-DomainListCSV = csv.reader(domains)
-DomainList = [column[1] for column in DomainListCSV]
+#pull only the second column from the csv file while using csv.reader module
+DomainList = [column[1] for column in csv.reader(domains)]
+#use strip to remove the right /
 StripList = [item.rstrip('/') for item in DomainList]
+#print the output on each new line
 print('\n'.join(StripList))
 
