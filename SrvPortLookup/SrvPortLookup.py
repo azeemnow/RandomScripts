6a9497a96ebe2ef59bab9421764f374ba307dc44
azeemnow <nowazeem@gmail.com>
2016-05-07 14:18:43 -0400

    Delete SrvPortLookup.py

--- SrvPortLookup/SrvPortLookup.py
+++
@@ -1,2 +0,0 @@
-This scripts includes port and service information from: http://ciscotut.com/pdf/common_ports.pdf. It allows you to lookup either port number or service name and in return it prints out the corresponding value. For example, if 22 is entered the script output will be SSH and vice versa. 
-Please note that this script is written using Python version 2.7; you may experience errors while running on a different version.
